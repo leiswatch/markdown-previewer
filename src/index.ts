f74d24258dbbf65921687502246fd7cb1ee45923
Michael Kowalewski <leiswatch@tutanota.com>
2019-12-17 18:45:12 +0100

    Add editor, preview and styling

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,19 @@ import marked from 'marked';
 
 const editor: HTMLTextAreaElement = document.querySelector('.markdown-container__editor');
 const preview: HTMLParagraphElement = document.querySelector('.markdown-container__preview');
+const initialEditorValue: string = `# h1
+## h2
+### h3
+#### h4
+`;
 
-function updatePreview(event: Event): void {
+const updatePreview = (event: Event): void => {
   preview.innerHTML = marked((<HTMLTextAreaElement>event.target).value);
-}
+};
 
 editor.addEventListener('input', updatePreview);
+
+window.addEventListener('load', () => {
+  editor.innerHTML = initialEditorValue;
+  preview.innerHTML = marked(editor.innerHTML);
+});
